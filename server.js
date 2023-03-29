85db72606b50c45d2ae47889e5ed2b783fce2072
Jinhyoung Lee <jhcode0@naver.com>
2023-03-29 14:38:10 +0900

    merge / makeExecutableSchema

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import {ApolloServer} from 'apollo-server';
-import {typeDefs,resolvers} from './shema';
+import schema from './shema';
 
 
 const server = new ApolloServer({
-    typeDefs,resolvers
+    schema
 })
 
 server.listen(4000).then(()=>console.log('http://localhost:4000👌'))
--- shema.js
+++ shema.js
@@ -1,61 +1,12 @@
-import { gql} from "apollo-server";
-import client from './client';
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeTypeDefs,mergeResolvers } from "@graphql-tools/merge";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
-export const typeDefs = gql`
-    type Movie {
-        id:Int!
-        title:String!
-        year:Int!
-        gener:String
-        createdAt:String!
-        updatedAt:String!
-    }
-    type Query {
-        movies: [Movie]
-        movie(id: Int!): Movie
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const typeDefs = mergeTypeDefs(loadedTypes);
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.{mutation,queries}.js`);
+const resolvers = mergeResolvers(loadedResolvers);
 
-    }
-    type CreateResult {
-        ok:Boolean
-        error:String
-    }
-    type Mutation {
-        createMovie(title: String!, year: Int!, genre: String): Movie
-        deleteMovie(id:Int!):CreateResult
-    }
-    
-`;
-export const resolvers = {
-    Query: {
-        movies: ()=>client.movie.findMany(),
-        movie: (_, { id }) => client.movie.findUnique({where:{id:id}}),
-    },
-    Mutation: {
-        createMovie:(_,{title,year,gener})=>{
-            //console.log('create',title,year,gener);
-            return client.movie.create({data:{title,year,gener}})
-            
-        },
-        deleteMovie:async(_,{id})=> {
-            //console.log('del',title,year,gener);
-            console.log(id)
-            const row = await client.movie.findUnique({
-                where: {
-                  id: id,
-                },
-              })
-              console.log(id)
-              
-            console.log(row);
-            if(!row){
-                
-                 throw console.error("없어영");
-            }
-            return client.movie.delete({where:{id:id}})
-            
-            
-        }
-    }
-
-}
+const schema = makeExecutableSchema({typeDefs,resolvers})
+export default schema;
 
