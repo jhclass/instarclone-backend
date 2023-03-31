e9f1ccad0c181be5a0161ab8d4492bd491c35ae1
Jinhyoung Lee <jhcode0@naver.com>
2023-03-31 15:42:47 +0900

    context

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ import schema from './shema';
 
 
 const server = new ApolloServer({
-    schema
+    schema,
+    context: {
+        "token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjgwMjM2ODU3LCJleHAiOjE2ODE0NDY0NTd9.IO2CftSB9Bjif_EspBWOM0ud601SNWAexjAeYoasvkc"
+    }
 })
 
 server.listen(process.env.PORT).then(()=>console.log(`http://localhost:${process.env.PORT}👌`))
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,18 +1,25 @@
 import client from "../../client"
 import bcypt from "bcrypt"
+import jwt from "jsonwebtoken"
 export default {
     Mutation:{
         //update Pofile
         //first. First of all, proceed under the premise that the id is known. 
         //second. When changing a password, it is hashed and updated.
-
+        //add option : Do have token or not. Maybe this is real first better than others. 
+        
         editProfile:async(_,{
             firstName,
             lastName,
             username,
             email,
             password:newPassword,
-        })=> {
+            
+        },context)=> {
+            //console.log('token:',token) 
+            const token = context.token;
+            const verifiedId = jwt.verify(token,process.env.SECRET_KEY)
+            console.log(verifiedId)
             let uglyPassword = null;
             if (newPassword) {
                 uglyPassword = await bcypt.hash(newPassword,10)
@@ -21,7 +28,7 @@ export default {
             //console.log(uglyPassword)
             const ok = await client.user.update({
                 where:{
-                    id:1,
+                    id:verifiedId.id,
                 },
                 data:{
                     firstName,
