106647cb1192e6bd91ee69e9563d1c6291226e6d
Jinhyoung Lee <jhcode0@naver.com>
2023-03-31 13:18:45 +0900

    Exit edit Profile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,6 +1,46 @@
-import client from "../../client";
+import client from "../../client"
+import bcypt from "bcrypt"
 export default {
     Mutation:{
-        editProfile:()=> console.log('hi')
+        //update Pofile
+        //first. First of all, proceed under the premise that the id is known. 
+        //second. When changing a password, it is hashed and updated.
+
+        editProfile:async(_,{
+            firstName,
+            lastName,
+            username,
+            email,
+            password:newPassword,
+        })=> {
+            let uglyPassword = null;
+            if (newPassword) {
+                uglyPassword = await bcypt.hash(newPassword,10)
+            }
+            //console.log(newPassword)
+            //console.log(uglyPassword)
+            const ok = await client.user.update({
+                where:{
+                    id:1,
+                },
+                data:{
+                    firstName,
+                    lastName,
+                    username,
+                    email,
+                    ...(newPassword&&{password:uglyPassword})
+                },
+            })
+            if(ok){
+                return {
+                    ok:true
+                }
+            }else{
+                return {
+                    ok:false,
+                    error:"Counld not edit profile."
+                }
+            }
+        }
     }
 }
