cb65c4587481aaefcfa740e134db3f3cf7663ba2
Jinhyoung Lee <jhcode0@naver.com>
2023-03-31 15:53:08 +0900

    {req} in context

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ import schema from './shema';
 
 const server = new ApolloServer({
     schema,
-    context: {
-        "token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjgwMjM2ODU3LCJleHAiOjE2ODE0NDY0NTd9.IO2CftSB9Bjif_EspBWOM0ud601SNWAexjAeYoasvkc"
+    context: ({req})=> {
+        //console.log(req.headers.token)
+        return {
+        "token":req.headers.token
+        }
     }
 })
 
