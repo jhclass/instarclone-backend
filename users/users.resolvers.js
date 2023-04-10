9961752bef0784a2022e0fa613668832c11c8107
Jinhyoung Lee <jhcode0@naver.com>
2023-04-10 20:20:56 +0900

    Computed fields (isMe/isFollowing)

--- users/users.resolvers.js
+++ users/users.resolvers.js
@@ -24,7 +24,25 @@ export default {
                     }
                 }
             });
+        },
+        isMe:({username},_,context)=>{
+            //console.log(username,context.loggedInUser)
+            if(username==context.loggedInUser.username){
+                return true
+            }else{
+                return false
+            }
+        },
+        isFollowing:async({username},_,{loggedInUser})=>{ //username을 userId 로 생각해
+            if(!loggedInUser){
+                return false
+            }
+            const exists = await client.user
+            .findUnique({where:{username:loggedInUser.username}})//로그인한 username 을 찾고
+            .following({where:{username}})//following 하고 있는지
+            return exists.length !== 0 //true false 를 상황에 따라 반환하겠지
         }
+
         
     }
 }
--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -16,9 +16,10 @@ export default gql`
         follower: [User]
         totalFollowing:Int!
         totalFollower:Int!
-      
+        isMe:Boolean!
+        isFollowing:Boolean!
         }
 `;
 
-//isFollowing:Boolean
-//isMe:Boolean
+//isFollowing: 이 계정을 내가 팔로잉 하고 있니?
+//isMe: 이 계정이 나의 계정이 맞니?
