0423fc86ce1c9f7db4b68d941cbb4856554dfa4a
Jinhyoung Lee <jhcode0@naver.com>
2023-04-11 13:04:33 +0900

    add throw Error & paginaion

--- users/searchUsers/searchUsers.resolvers.js
+++ users/searchUsers/searchUsers.resolvers.js
@@ -1,7 +1,7 @@
 import client from "../../client"
 export default {
     Query: {
-        searchUsers:async(_,{keyword})=>{
+        searchUsers:async(_,{keyword,lastId})=>{
             try {
                 if (keyword.length<1){
                     throw new Error("키워드가 너무 잛음")
@@ -9,10 +9,15 @@ export default {
                 const searchResult = await client.user.findMany({where:{
                         username: {
                             //startsWith:keyword.toLowerCase(),
-                            contains:keyword.toLowerCase(),
+                            contains:keyword.toLowerCase(), // 동일한 글자가 (말그대로 포함) 있다면
                             //mode:"insensitive", //이걸 쓰면 .toLowerCase 를 사용할 필요가 없지
-                        }
-                    }})
+                        },
+                    },
+                    take:3,
+                    skip:1,
+                    ...(lastId&&{cursor:{id:lastId}})
+
+                })
                 return searchResult
                 
             } catch (error) {
