a79a8e304766aef43ff9ee378e990bb8669b4b34
Jinhyoung Lee <jhcode0@naver.com>
2023-04-12 18:24:10 +0900

    hashtags of Photo(model)

--- photos/photos.resolvers.js
+++ photos/photos.resolvers.js
@@ -3,8 +3,21 @@ export default {
     Photo: {
         user: ({ userId }) => {
             //console.log(root)
-            //root 의 userId (Photo model 의 정보로 user 정보를 찾아라)
+            //root 의 userId (Photo model 의 정보로 user 정보를 찾아라!)
             return client.user.findUnique({ where: { id: userId } })
+        },
+        hashtags: ({ id }) => {
+            return client.hashtag.findMany({
+                where: {
+                    photos: {
+                        some: {
+                            id
+                        }
+                    }
+
+                }
+            })
         }
     }
+
 }
