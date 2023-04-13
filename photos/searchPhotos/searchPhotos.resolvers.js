92248adbd00bec264b77bcd4dd53e2380b826b9b
Jinhyoung Lee <jhcode0@naver.com>
2023-04-13 13:51:58 +0900

    searchPhotos (modified)

--- photos/searchPhotos/searchPhotos.resolvers.js
+++ photos/searchPhotos/searchPhotos.resolvers.js
@@ -3,7 +3,7 @@ import client from "../../client"
 export default {
     Query: {
         searchPhotos: (_, { keyword }) => {
-            client.photo.findMany({
+            return client.photo.findMany({
                 where: {
                     caption: {
                         startsWith: keyword
