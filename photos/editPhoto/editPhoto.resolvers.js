66e4fca06ec7a7e65130c1831ecf86a3ba428bf6
Jinhyoung Lee <jhcode0@naver.com>
2023-04-13 16:01:25 +0900

    editPhoto -1

--- photos/editPhoto/editPhoto.resolvers.js
+++ photos/editPhoto/editPhoto.resolvers.js
@@ -1,3 +1,36 @@
+import { protectedResolver } from "../../users/users.utils";
+import client from "../../client";
 export default {
+    Mutation: {
+        editPhoto: protectedResolver(async (_, { id, caption }, context) => {
+            const photo = await client.photo.findUnique({
+                where: { id }, //photo's id
+            })
+            console.log(photo)
+            //포토의 userId === loggedInUser 의 아이디가 같은지
+            if (photo.userId !== context.loggedInUser.id) {
+                return {
+                    ok: false,
+                    error: "본인의 게시물만 수정할 수 있습니다."
+                }
 
+            } else {
+                const ok = await client.photo.update({
+                    where: {
+                        id
+                    }, data: {
+                        caption: caption
+                    }
+                })
+                if (ok) {
+                    return {
+                        ok: true,
+                        changes: caption
+                    }
+                }
+            }
+            //같다면 해시태그 수정
+            //같지 않으면 false, error message 리턴.
+        })
+    }
 }
--- photos/editPhoto/editPhoto.typeDefs.js
+++ photos/editPhoto/editPhoto.typeDefs.js
@@ -1,7 +1,14 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
-    type Query {
-        editPhoto(file:String!):User
+    type EditPhotoResult {
+        ok:Boolean!
+        error:String
+        changes:String
+    }
+    type Mutation {
+        editPhoto(id:Int!,caption:String!):EditPhotoResult!
     }
 `;
+
+//인스타에서는 캡션만 수정이 가능하지
