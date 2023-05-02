48e8a6c23f9c8c57c94935e3d0a5332828849b90
Jinhyoung Lee <jhcode0@naver.com>
2023-05-02 17:11:18 +0900

    Filtering Subscription -2

--- subscription/subscription.resolvers.js
+++ subscription/subscription.resolvers.js
@@ -1,24 +1,30 @@
 import { withFilter } from "graphql-subscriptions";
 import pubsub from "../pubsub";
+import client from "../client";
+
 export default {
   Subscription: {
-
     newMessage: {
-      // More on pubsub below
-
-      subscribe:
-        withFilter(
+      subscribe: async (root, args, context, info) => {
+        const room = await client.room.findUnique({
+          where: {
+            id: args.id,
+          },
+          select: {
+            id: true,
+          },
+        });
+        if (!room) {
+          //console.log("룸없음!")
+          throw new Error("Room not found.");
+        }
+        return withFilter(
           () => pubsub.asyncIterator("NEW_MESSAGE"),
           (payload, variables) => {
-            console.log(payload, variables)
-            return true;
-
-
+            return payload.newMessage.roomId === variables.id;
           }
-        )
-
+        )(root, args, context, info);
+      },
     },
-
   },
-  // ...other resolvers...
-}
+};
