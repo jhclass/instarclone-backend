a90e8b1d46157762486826bc1fd02fdf5c9488a1
Jinhyoung Lee <jhcode0@naver.com>
2023-05-25 16:17:00 +0900

    create 'me' -2

--- users/me/me.resolvers.js
+++ users/me/me.resolvers.js
@@ -1,12 +1,15 @@
 import { protectedResolver } from "../users.utils";
+import client from "../../client";
 export default {
   Query: {
-    me: protectedResolver((_, __, context) => {
-      return client.user.findUnique({
+    me: protectedResolver(async (_, __, context) => {
+      const iamuser = await client.user.findUnique({
         where: {
           id: context.loggedInUser,
         },
       });
+      console.log(iamuser);
+      return iamuser;
     }),
   },
 };
