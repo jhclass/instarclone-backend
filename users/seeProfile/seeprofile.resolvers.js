8e7d0fd00076e8e0f8a567588afc2d9f69282ea8
Jinhyoung Lee <jhcode0@naver.com>
2023-06-15 19:21:47 +0900

    Modify seeProfile.resolvers

--- users/seeProfile/seeprofile.resolvers.js
+++ users/seeProfile/seeprofile.resolvers.js
@@ -1,22 +1,16 @@
 import client from "../../client";
-import { protectedResolver } from "../users.utils";
+
 export default {
-    Query: {
-        seeProfile: protectedResolver(
-            async (_, { username }) => {
-                //findUnique : find @unique in User model
-                const findUser = await client.user.findUnique({
-                    where: { username: username },
-                    include: {
-                        following: true,
-                        follower: true
-                    }
-                });
-                console.log(findUser.createdAt);
-                return findUser;
-
-            }
-        )
-
-    }
-}
+  Query: {
+    seeProfile: (_, { username }) =>
+      client.user.findUnique({
+        where: {
+          username,
+        },
+        include: {
+          following: true,
+          follower: true,
+        },
+      }),
+  },
+};
--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -1,25 +1,25 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
-    type User {
-        id: Int! 
-        firstName: String!
-        lastName: String
-        username: String! 
-        email: String!
-        password: String!
-        bio:String
-        avatar:String
-        createdAt: String!
-        UpdatedAt: String!
-        following: [User]
-        follower: [User]
-        totalFollowing:Int!
-        totalFollower:Int!
-        isMe:Boolean!
-        isFollowing:Boolean!
-        photos(lastId:Int):[Photo]
-        }
+  type User {
+    id: Int!
+    firstName: String!
+    lastName: String
+    username: String!
+    email: String!
+    password: String!
+    bio: String
+    avatar: String
+    createdAt: String!
+    UpdatedAt: String!
+    following: [User]
+    follower: [User]
+    totalFollowing: Int!
+    totalFollower: Int!
+    isMe: Boolean!
+    isFollowing: Boolean!
+    photos: [Photo]
+  }
 `;
 
 //isFollowing: 이 계정을 내가 팔로잉 하고 있니?
