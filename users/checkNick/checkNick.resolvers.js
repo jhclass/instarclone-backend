6002ea8a8ca5d496eaa6f040b6e55e2049186ac9
Jinhyoung Lee <jhcode0@naver.com>
2023-07-04 14:42:27 +0900

    modify 'Check user'

--- users/checkNick/checkNick.resolvers.js
+++ users/checkNick/checkNick.resolvers.js
@@ -1,20 +1,30 @@
+import client from "../../client";
 export default {
   Query: {
     checkNick: async (_, { nick }) => {
-      const checkNick = await client.user.count({
-        where: {
-          firstName: nick,
-        },
-      });
-      if (checkNick > 0) {
-        return {
-          ok: false,
-          message: " 이미 존재하는 이름 또는 닉네임 입니다.",
-        };
+      const regex = /^[A-Za-z0-9]+$/;
+      const isEnglish = regex.test(nick);
+      if (isEnglish) {
+        const checkNick = await client.user.count({
+          where: {
+            firstName: nick,
+          },
+        });
+        if (checkNick > 0) {
+          return {
+            ok: false,
+            message: " 이미 존재하는 이름 또는 닉네임 입니다.",
+          };
+        } else {
+          return {
+            ok: true,
+            message: " 사용가능한 이름 또는 닉네임 입니다.",
+          };
+        }
       } else {
         return {
-          ok: true,
-          message: " 사용가능한 이름 또는 닉네임 입니다.",
+          ok: false,
+          message: "영어(대,소문자),한글,숫자만 사용가능합니다.",
         };
       }
     },
--- users/checkNick/checkUser.resolvers.js
+++ users/checkNick/checkUser.resolvers.js
@@ -1,20 +1,30 @@
+import client from "../../client";
 export default {
   Query: {
     checkUser: async (_, { temporaryUser }) => {
-      const checkNick = await client.user.count({
-        where: {
-          firstName: temporaryUser,
-        },
-      });
-      if (checkNick > 0) {
-        return {
-          ok: false,
-          message: " 이미 존재하는 이름 또는 닉네임 입니다.",
-        };
+      const regex = /^[A-Za-z0-9]+$/;
+      const isEnglish = regex.test(temporaryUser);
+      if (isEnglish) {
+        const checkNick = await client.user.count({
+          where: {
+            username: temporaryUser,
+          },
+        });
+        if (checkNick > 0) {
+          return {
+            ok: false,
+            message: " 이미 존재하는 아이디 입니다.",
+          };
+        } else {
+          return {
+            ok: true,
+            message: " 사용가능한 아이디 입니다.",
+          };
+        }
       } else {
         return {
-          ok: true,
-          message: " 사용가능한 이름 또는 닉네임 입니다.",
+          ok: false,
+          message: "영어 대소문자와 숫자로만 구성된 아이디를 입력해주세요.",
         };
       }
     },
